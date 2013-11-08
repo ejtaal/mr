716f0f2d04a0d5741a432288bbddae36aea23e78
Mr. T <taal@camel>
2013-11-08 01:14:48 +0000

    Reworking AA html into more useful framework, work in progress...

---
+++ mawrid-conf.js
@@ -0,0 +1,35 @@
+var presets = {
+	"default": {
+		"name": "Default",
+		"order": "kz,mb"
+	},
+	"fr": {
+		"name": "French",
+		"order": "kz,mb"
+	},
+	"my": {
+		"name": "Malay",
+		"order": "mb,kz"
+	}
+}
+
+var books = {
+	"kz": {
+		"index-js": "mw-kz-index.js",
+		"color": "Red",
+		"direction": "ltr",
+		"columns": "2",
+		"offset": 20,
+		"startpage": 20,
+		"name": "Kazirski"
+	},
+	"mr": {
+		"index-js": "mw-mr-index.js",
+		"color": "Blue",
+		"direction": "rtl",
+		"columns": "2",
+		"offset": 30,
+		"startpage": 30,
+		"name": "Marbawi"
+	}
+}
