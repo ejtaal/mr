c277f7e53cc36f858829e1e4dbbb5820673fbb4b
Mr. T <taal@camel>
2013-11-23 17:46:21 +0000

    adapted for new format

---
+++ check-indexes.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+BASE=/tmp/mr-index-checks/
+mkdir -p "$BASE"
+
+for indexfile in mr-??-indexes.js; do
+	for index in `grep '\[\]' $indexfile | cut -d= -f1 | cut -d\  -f2 | sort`; do
+		grep "^$index\[[0-9]*\]\ *=" $indexfile | \
+			awk -F '"' '{ if ($2 != "") print $2 "|" $1 }' | grep . > "$BASE/${index}.orig"
+		sort "$BASE/${index}.orig" > "$BASE/${index}.sorted"
+		echo "Comparison for file '${index}':"
+		echo "======================================="
+		if cmp -s "$BASE/${index}.orig" "$BASE/${index}.sorted"; then
+			echo "Index of $(wc -l < $BASE/${index}.orig) lines looks well sorted and OK!! :)"
+		else
+			echo "Problems found in this index at the following locations:"
+			diff -u "$BASE/${index}.orig" "$BASE/${index}.sorted" | \
+				grep "^-[^-]" | sed 's/^-//g' | awk -F '|' '{ print $2 $1 }'
+		fi
+	done
+done
